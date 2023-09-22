180cb2f480788e9e33dec64680607d34e30da85e
William Horowitz <whoro1@unh.newhaven.edu>
2023-09-22 15:36:20 -0400

    Changed text

--- app/src/main/java/com/example/icebreaker_class_f23/MainActivity.kt
+++ app/src/main/java/com/example/icebreaker_class_f23/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.icebreaker_class_f23
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView : TextView
+    @SuppressLint("MissingInflatedId")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.welcomeText)
+
+        textView.setText("What's up class!")
     }
 }
