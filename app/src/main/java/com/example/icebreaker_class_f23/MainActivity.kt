2568f576dd8db803704fbb60bce41c4e10cec33b
William Horowitz <whoro1@unh.newhaven.edu>
2023-09-26 13:41:58 -0400

    Linked View to Model Through Controller
    Button press is captures and actions are preformed

--- app/src/main/java/com/example/icebreaker_class_f23/MainActivity.kt
+++ app/src/main/java/com/example/icebreaker_class_f23/MainActivity.kt
@@ -3,16 +3,19 @@ package com.example.icebreaker_class_f23
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import com.example.icebreaker_class_f23.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val TAG = "IcebreakerAndroidF23Tag"
 
     @SuppressLint("MissingInflatedId")
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -20,7 +23,20 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
+        binding.btnGetQuestion.setOnClickListener {
 
+            Log.d(TAG, "Get question button was pressed")
+            binding.txtAnswer.setText("Button was Clicked")
+
+        }
+
+
+
+    }
+
+    override fun onDestroy() {
+
+        super.onDestroy()
 
     }
 }
